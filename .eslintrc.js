01b8e61a01af9c4abdb965e80a66c70623975111
Marouane Faris <faris.marouane@gmail.com>
2019-10-13 20:38:56 +0200

    Remove prettier's useTabs rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
   },
-	extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2018,
   },
   rules: {
-		"prettier/prettier": [
-      "error",
-      { singleQuote: true, trailingComma: "all", useTabs: true, tabWidth: 2 }
-    ]
-  }
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        /* useTabs: true,*/
+        tabWidth: 2,
+      },
+    ],
+  },
 };

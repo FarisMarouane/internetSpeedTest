3f55cb84f5c90d23283504eb8b96748258dc7cd0
Marouane Faris <faris.marouane@gmail.com>
2019-10-29 16:43:14 +0100

    Make one request function

--- downloadSpeed.js
+++ downloadSpeed.js
@@ -6,70 +6,16 @@ function checkDownloadSpeed(urls) {
   let promiseArr = [];
 
   promiseArr = urls.map(url => {
-    return new Promise((resolve, reject) => {
-      let startTime;
-
-      if (!url.includes('https')) {
-        http.get(url, response => {
-          if (response.statusCode !== 200) {
-            const error = new Error(
-              'The speed test has failed',
-              response.statusCode,
-            );
-            reject(error);
-          }
-
-          response.once('data', () => {
-            startTime = new Date().getTime();
-          });
-
-          const size = response.headers['content-length'];
-
-          response.once('end', () => {
-            const endTime = new Date().getTime();
-            const duration = (endTime - startTime) / 1000;
-            const bitsLoaded = size * 8;
-            const bps = (bitsLoaded / duration).toFixed(2);
-            const kbps = (bps / 1024).toFixed(2);
-            const mbps = +(kbps / 1024).toFixed(2);
-
-            resolve(mbps);
-          });
-        });
-      } else {
-        https.get(url, response => {
-          if (response.statusCode !== 200) {
-            const error = new Error(
-              'The speed test has failed',
-              response.statusCode,
-            );
-            reject(error);
-          }
-
-          response.once('data', () => {
-            startTime = new Date().getTime();
-          });
-
-          const size = response.headers['content-length'];
-
-          response.once('end', () => {
-            const endTime = new Date().getTime();
-            const duration = (endTime - startTime) / 1000;
-            const bitsLoaded = size * 8;
-            const bps = (bitsLoaded / duration).toFixed(2);
-            const kbps = (bps / 1024).toFixed(2);
-            const mbps = +(kbps / 1024).toFixed(2);
-
-            resolve(mbps);
-          });
-        });
-      }
-    });
+    return new Promise((resolve, reject) => makeRequest(url, resolve, reject));
   });
 
   return Promise.all(promiseArr)
     .then(arr => {
-      const average = (arr[0] + arr[1]) / 2;
+      console.log('Arr all: ', arr);
+      const average =
+        arr.reduce((acc, curr) => {
+          return acc + curr;
+        }) / arr.length;
       return average.toFixed();
     })
     .catch(e => {
@@ -78,3 +24,41 @@ function checkDownloadSpeed(urls) {
 }
 
 module.exports = checkDownloadSpeed;
+
+function makeRequest(url, resolve, reject) {
+  let startTime;
+  (!url.includes('https') ? http : https).get(url, response => {
+    if (response.statusCode !== 200) {
+      const error = new Error('The speed test has failed', response.statusCode);
+      reject(error);
+    }
+
+    const arr = [];
+
+    response.once('data', () => {
+      startTime = new Date().getTime();
+    });
+
+    // Not all servers (due to availability) have files of the same sizes, hence the need to standarize
+    response.on('data', chunk => {
+      if (arr.length <= 500000000) {
+        // 500 Mo
+        arr.push(chunk);
+        return;
+      }
+      response.destroy();
+    });
+
+    response.once('end', () => {
+      const buffer = Buffer.concat(arr);
+      const endTime = new Date().getTime();
+      const duration = (endTime - startTime) / 1000;
+      const bitsLoaded = buffer.length;
+      const bps = (bitsLoaded / duration).toFixed(2);
+      const kbps = (bps / 1024).toFixed(2);
+      const mbps = +(kbps / 1024).toFixed(2);
+
+      resolve(mbps);
+    });
+  });
+}

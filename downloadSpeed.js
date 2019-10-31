5d2215ad672e7b37a2311529eb0c0279c0585ab7
faris <marouane.faris@cpexterne.org>
2019-10-31 16:10:58 +0100

    Take into account pascal's feedback

--- downloadSpeed.js
+++ downloadSpeed.js
@@ -9,9 +9,7 @@ function checkDownloadSpeed(urls) {
   let counter = 0;
 
   promiseArr = urls.map(url => {
-    return new Promise((resolve, reject) =>
-      makeRequest(url, counter, resolve, reject),
-    );
+    return makeRequest(url, counter++);
   });
 
   return Promise.all(promiseArr)
@@ -30,59 +28,62 @@ function checkDownloadSpeed(urls) {
 
 module.exports = checkDownloadSpeed;
 
-function makeRequest(url, counter, resolve, reject) {
+function makeRequest(url, counter) {
   let startTime;
-  (!url.includes('https') ? http : https).get(url, response => {
-    if (response.statusCode !== 200) {
-      const error = new Error('The speed test has failed', response.statusCode);
-      reject(error);
-    }
-
-    const arr = [];
-
-    response.once('data', () => {
-      startTime = new Date().getTime();
-    });
-
-    // Not all servers (due to availability) have files of the same sizes, hence the need to standarize
-    response.on('data', chunk => {
-      switch (counter) {
-        case 0:
-          if (arr.length <= thresholds[0] * 1000000) {
-            // Times 1 million to get MB
-            arr.push(chunk);
-            return;
-          }
-          break;
-        case 1:
-          if (arr.length <= thresholds[1] * 1000000) {
-            arr.push(chunk);
-            return;
-          }
-          break;
-        case 2:
-          if (arr.length <= thresholds[2] * 1000000) {
-            arr.push(chunk);
-            return;
-          }
-          break;
-        default:
-          break;
+  return new Promise((resolve, reject) => {
+    (url.includes('https') ? https : http).get(url, response => {
+      if (response.statusCode !== 200) {
+        const error = new Error(
+          'The speed test has failed',
+          response.statusCode,
+        );
+        reject(error);
       }
-      response.destroy();
-    });
 
-    response.once('end', () => {
-      const buffer = Buffer.concat(arr);
-      const endTime = new Date().getTime();
-      const duration = (endTime - startTime) / 1000;
-      const bitsLoaded = buffer.length;
-      const bps = (bitsLoaded / duration).toFixed(2);
-      const kbps = (bps / 1024).toFixed(2);
-      const mbps = +(kbps / 1024).toFixed(2);
+      const arr = [];
 
-      resolve(mbps);
+      response.once('data', () => {
+        startTime = new Date().getTime();
+      });
+
+      // Not all servers (due to availability) have files of the same sizes, hence the need to standarize
+      response.on('data', chunk => {
+        switch (counter) {
+          case 0:
+            if (Buffer.concat(arr).length <= thresholds[0] * 1000000) {
+              // Times 1 million to get the number of bytes in millions
+              arr.push(chunk);
+              return;
+            }
+            break;
+          case 1:
+            if (Buffer.concat(arr).length <= thresholds[1] * 1000000) {
+              arr.push(chunk);
+              return;
+            }
+            break;
+          case 2:
+            if (Buffer.concat(arr).length <= thresholds[2] * 1000000) {
+              arr.push(chunk);
+              return;
+            }
+            break;
+          default:
+            break;
+        }
+        response.destroy();
+      });
+
+      response.once('end', () => {
+        const buffer = Buffer.concat(arr);
+        const endTime = new Date().getTime();
+        const duration = (endTime - startTime) / 1000;
+        const bitsLoaded = buffer.length * 8;
+        const bps = (bitsLoaded / duration).toFixed(2);
+        const kbps = (bps / 1024).toFixed(2);
+        const mbps = +(kbps / 1024).toFixed(2);
+        resolve(mbps);
+      });
     });
   });
-  counter++;
 }

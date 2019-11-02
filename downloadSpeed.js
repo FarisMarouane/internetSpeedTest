f195c591c0225feeb07b6e0037d4d31d54360f03
Marouane Faris <faris.marouane@gmail.com>
2019-11-02 22:09:17 +0100

    Take into account pascal's comments

--- downloadSpeed.js
+++ downloadSpeed.js
@@ -7,15 +7,13 @@ const NUMBER_OF_REQUESTS = 1;
 // Returns an average speed (if all goes well!)
 function checkDownloadSpeed(urls) {
   let promiseArr = [];
-  let counter = 0;
 
-  promiseArr = urls.slice(0, NUMBER_OF_REQUESTS).map(url => {
-    return makeRequest(url, counter++);
+  promiseArr = urls.slice(0, NUMBER_OF_REQUESTS).map((url, i) => {
+    return makeRequest(url, i);
   });
 
   return Promise.all(promiseArr)
     .then(arr => {
-      console.log('Arr all: ', arr);
       const average =
         arr.reduce((acc, curr) => {
           return acc + curr;
@@ -26,8 +24,6 @@ function checkDownloadSpeed(urls) {
       throw new Error('checkDownloadSpeed: ', e);
     });
 }
-
-module.exports = checkDownloadSpeed;
 
 function makeRequest(url, counter) {
   let startTime;
@@ -44,15 +40,11 @@ function makeRequest(url, counter) {
       const arr = [];
 
       response.once('data', () => {
-        console.log(`Url ${url} started receiving data`);
         startTime = new Date().getTime();
       });
 
       // Not all servers (due to availability) have files of the same sizes, hence the need to standarize
       response.on('data', chunk => {
-        if (url === 'http://ping.online.net/500Mo.dat') {
-          // console.log('500 MB url is receiving data');
-        }
         switch (counter) {
           case 0:
             if (Buffer.concat(arr).length <= thresholds[0] * 1_000_000) {
@@ -70,16 +62,9 @@ function makeRequest(url, counter) {
           case 2: {
             if (Buffer.concat(arr).length <= thresholds[2] * 1_000_000) {
               // 3 000 000
-              // console.log('500 MB threshold not passed');
               arr.push(chunk);
               return;
             }
-            console.log('100 MB threshold PASSED!');
-            const thresholdIntervalId = setInterval(() => {
-              return console.log('100 MB threshold PASSED!');
-            }, 1500);
-
-            setTimeout(() => clearInterval(thresholdIntervalId), 15_000);
             break;
           }
 
@@ -103,3 +88,5 @@ function makeRequest(url, counter) {
     });
   });
 }
+
+module.exports = checkDownloadSpeed;

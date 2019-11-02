ed9a58b1f5f932a5a0fd8dbb4231d5cdd1d7b6c5
Marouane Faris <faris.marouane@gmail.com>
2019-11-02 03:55:04 +0100

    Change number of requests and file limit size

--- downloadSpeed.js
+++ downloadSpeed.js
@@ -1,14 +1,15 @@
 const https = require('https');
 const http = require('http');
 
-const thresholds = [10, 100, 500];
+const thresholds = [20, 20, 20];
+const NUMBER_OF_REQUESTS = 1;
 
 // Returns an average speed (if all goes well!)
 function checkDownloadSpeed(urls) {
   let promiseArr = [];
   let counter = 0;
 
-  promiseArr = urls.map(url => {
+  promiseArr = urls.slice(0, NUMBER_OF_REQUESTS).map(url => {
     return makeRequest(url, counter++);
   });
 
@@ -43,31 +44,45 @@ function makeRequest(url, counter) {
       const arr = [];
 
       response.once('data', () => {
+        console.log(`Url ${url} started receiving data`);
         startTime = new Date().getTime();
       });
 
       // Not all servers (due to availability) have files of the same sizes, hence the need to standarize
       response.on('data', chunk => {
+        if (url === 'http://ping.online.net/500Mo.dat') {
+          // console.log('500 MB url is receiving data');
+        }
         switch (counter) {
           case 0:
-            if (Buffer.concat(arr).length <= thresholds[0] * 1000000) {
+            if (Buffer.concat(arr).length <= thresholds[0] * 1_000_000) {
               // Times 1 million to get the number of bytes in millions
               arr.push(chunk);
               return;
             }
             break;
           case 1:
-            if (Buffer.concat(arr).length <= thresholds[1] * 1000000) {
+            if (Buffer.concat(arr).length <= thresholds[1] * 1_000_000) {
               arr.push(chunk);
               return;
             }
             break;
-          case 2:
-            if (Buffer.concat(arr).length <= thresholds[2] * 1000000) {
+          case 2: {
+            if (Buffer.concat(arr).length <= thresholds[2] * 1_000_000) {
+              // 3 000 000
+              // console.log('500 MB threshold not passed');
               arr.push(chunk);
               return;
             }
+            console.log('100 MB threshold PASSED!');
+            const thresholdIntervalId = setInterval(() => {
+              return console.log('100 MB threshold PASSED!');
+            }, 1500);
+
+            setTimeout(() => clearInterval(thresholdIntervalId), 15_000);
             break;
+          }
+
           default:
             break;
         }
@@ -75,6 +90,7 @@ function makeRequest(url, counter) {
       });
 
       response.once('end', () => {
+        console.log(`Url ${url} stopped receiving data`);
         const buffer = Buffer.concat(arr);
         const endTime = new Date().getTime();
         const duration = (endTime - startTime) / 1000;

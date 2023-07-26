c8bd3b06c1a04c0024f0ff36972d5862375c9776
Marouane Faris <faris.marouane@gmail.com>
2023-07-26 14:14:46 +0200

    Use regular Jxs instead of createElement

--- src/index.js
+++ src/index.js
@@ -2,19 +2,14 @@ import React from 'react';
 import { render } from 'ink';
 
 import { timeout } from './helpers';
-import ui from './ui';
+import Ui from './ui';
 
 const argument = process.argv[2];
 const TEST_MAX_DURATION = 30_000;
 const testTimeout = timeout(TEST_MAX_DURATION);
 
 const main = async () => {
-    const app = render(
-        React.createElement(ui, {
-            argument,
-            testTimeout,
-        }),
-    );
+    const app = render(<Ui argument={argument} testTimeout={testTimeout} />);
 
     await app.waitUntilExit();
     process.exit(0);
